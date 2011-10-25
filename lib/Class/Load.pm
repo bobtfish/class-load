d1b9460f8753ed4a7dbce7a7ba59500cf90d4814
Tomas Doran <bobtfish@bobtfish.net>
2011-10-24 18:47:24 -0700

    Require the latest version of Module::Runtime

--- lib/Class/Load.pm
+++ lib/Class/Load.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use base 'Exporter';
 use Data::OptList 'mkopt';
-use Module::Runtime qw(check_module_name module_notional_filename
+use Module::Runtime 0.011 qw(check_module_name module_notional_filename
                        require_module use_module);
 use Package::Stash;
 use Try::Tiny;
